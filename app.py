693ae6a02680d1178304c55440fd23aaf396037a
wskang-beep <wskang@coway.com>
2026-03-25 17:24:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+def local_css(file_name):
+    with open(file_name, encoding="utf-8") as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# CSS 파일 적용
+local_css("style.css")
+
 # HTML 파일을 읽어서 Streamlit 화면에 뿌려주는 방식
 with open("index.html", "r", encoding="utf-8") as f:
     html_code = f.read()
